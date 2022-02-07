719a5d036df9094b7ac051e1347dedd4dd37a621
Mihir Waykole <76737051+Mihir1703@users.noreply.github.com>
2022-02-07 22:53:40 +0530

    Delete new.py

--- new.py
+++
@@ -1,6 +0,0 @@
-provider = input("Which password do you want.\n")
-dic = {"facebook":"abcdca","google":"987452155","instagram":"964260"}
-if provider.lower() in dic.keys(): 
-    print("Your ",provider.lower()," password is ",dic[provider.lower()])
-else :
-    print("Not available")
